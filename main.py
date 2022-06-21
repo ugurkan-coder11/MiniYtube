c6414ecf0efbd9b7c4319411b938ed9c1e4a017c
Uğurkan <107849533+ugurkan-coder11@users.noreply.github.com>
2022-06-21 14:52:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from youtubesearchpython import Search #pip install youtube-search-python
 from pytube import YouTube #pip install pytube
 from moviepy.editor import VideoFileClip #pip install moviepy
-import os
+import os,httpx
 
 class Engine:
     def __init__(self):
@@ -24,49 +24,62 @@ class Engine:
     
     def searchMotor(self):
         try:
-            arama_limiti = int(input("Arama Limitini giriniz: "))
-            self.search = input("Arama: ")
-            self.allSearch = Search(self.search, limit = arama_limiti)
-            results = self.allSearch.result()["result"]
-            for i in range(arama_limiti):
-                self.url = results[i]["link"]
-                self.title = results[i]["title"]
-                self.channel = results[i]["channel"]["name"]
-                view = results[i]["viewCount"]["text"]
-                print(f"[{i}] {self.title}, Channel: {self.channel}, VIEW: {view}")
-            print("-"*100)
-            selectt = int(input("Seçimin: "))
-            if selectt > arama_limiti:
-                print("### LİMİTİ AŞMA! ###")
-            else:
-                text_two = f"""
-                *********************************************************
-                ---------------------------------------------------------
-                Video:
-                    - Ismi : {results[selectt]['title']}
-                    - Kanal adı : {results[selectt]['channel']['name']}
-                    - Izlenme : {results[selectt]['viewCount']['text']}
-                    - URL : {results[selectt]["link"]}
-                Indirme:
-                    1 - Mp4 olarak indir
-                    2 - Mp3 olarak indir
-                    3 - Çıkış yap
-                ---------------------------------------------------------
-                *********************************************************
-                """
-                print(text_two)
-                download = int(input("Seçimin: "))
-                if download == 1:
-                    self.motor(results[selectt]["link"],"mp4")
-                    print("INDIRME ISLEMI BASARILI!")
-                elif download == 2:
-                    self.motor(results[selectt]["link"],"mp3")
-                    print("INDIRME ISLEMI BASARILI!")
-                elif download == 3:
-                    print("Yine Bekleriz..")
-                    exit()
-        except ValueError:
-            print("INT ERROR")
+            try:
+                arama_limiti = int(input("Arama Limitini giriniz: "))
+                self.search = input("Arama: ")
+                self.allSearch = Search(self.search, limit = arama_limiti)
+                results = self.allSearch.result()["result"]
+                try:
+                    for i in range(arama_limiti):
+                        title = results[i]["title"]
+                        channel = results[i]["channel"]["name"]
+                        view = results[i]["viewCount"]["text"]
+                        print(f"[{i}] {title}, Channel: {channel}, VIEW: {view}")
+                except IndexError:
+                    print("### DAHA FAZLA SECENEK BULUNMAMAKTADIR ###")
+                print("-"*100)
+                slt = int(input("Seçimin: "))
+                if slt > arama_limiti:
+                    print("### LİMİTİ AŞMA! ###")
+                else:
+                    text_two = f"""
+                    *********************************************************
+                    ---------------------------------------------------------
+                    Video:
+                        - Ismi : {results[slt]['title']}
+                        - Kanal adı : {results[slt]['channel']['name']}
+                        - Izlenme : {results[slt]['viewCount']['text']}
+                        - URL : {results[slt]["link"]}
+                    Indirme:
+                        1 - Mp4 olarak indir
+                        2 - Mp3 olarak indir
+                        3 - Çıkış yap
+                    ---------------------------------------------------------
+                    *********************************************************
+                    """
+                    print(text_two)
+                    information = f"Title: {results[slt]['title']}\nChannel: {results[slt]['channel']['name']}\nView: {results[slt]['viewCount']['text']}\nURL: {results[slt]['link']}"
+                    dosya = open(f"{results[slt]['title']}.txt","w",encoding="utf-8").write(str(information))
+                    while True:
+                        download = int(input("Seçimin: "))
+                        if download == 1:
+                            self.motor(results[slt]["link"],"mp4")
+                            print("INDIRME ISLEMI BASARILI!")
+                            break
+                        elif download == 2:
+                            self.motor(results[slt]["link"],"mp3")
+                            print("INDIRME ISLEMI BASARILI!")
+                            break
+                        elif download == 3:
+                            print("Yine Bekleriz..")
+                            break
+                        else:
+                            print("## YANLIS SECİM ##")
+            except ValueError:
+                print("INT ERROR")
+        except httpx.ConnectError:
+            os.system("cls")
+            print("Internet Bağlantınızı Kontrol Ediniz!")
 
     def motor(self,url,format = "mp4"):
         if format == "mp4":
